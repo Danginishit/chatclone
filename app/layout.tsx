77fe4c427fcf3dbbaaea406ec3949c358717a748
Nishit Dangi <danginishit114@gmail.com>
2024-02-21 23:58:05 +0530

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { useEffect } from 'react';
-import  '../components/MyDynamicComponent';
+import  '../components/MydynamicComponent';
 
 import "./globals.css";
 // import 'jquery';
