de985d0e30c4f02ca0439382cf01e83478de27bd
Nishit Dangi <danginishit114@gmail.com>
2024-02-24 16:13:07 +0530

    changes

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -108,7 +108,7 @@ export default function Home() {
             if(touserData){
                 receiveDataFromChild(touserData);
             }
-        },2000);
+        },5000);
         // console.log(apiData)
     }, [cookies?.user,touserData,userChat]);
 
